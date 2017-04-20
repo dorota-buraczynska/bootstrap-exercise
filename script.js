cf54a0e80626d7fa7e4daf5a6e32e176922da2cb
Dorota Buraczyńska <dorota.buraczynska@gmail.com>
2017-04-20 16:59:35 +0200

    add scrollSpy and scrollMenu function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function initMap() {
-    var position = {lat: 41.875329, lng:  -87.627811};
+    var position = {lat: 41.875329, lng: -87.627811};
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
         center: position,
@@ -16,11 +16,24 @@ function initMap() {
 }
 
 $(window).scroll(function () {
-   $('.slideanim').each(function () {
-     var position = $(this).offset().top;
-     var winTop = $(window).scrollTop();
-     if (position < winTop + 600) {
-         $(this).addClass('slide');
-     }
-   });
+    $('.slideanim').each(function () {
+        var position = $(this).offset().top;
+        var winTop = $(window).scrollTop();
+        if (position < winTop + 600) {
+            $(this).addClass('slide');
+        }
+    });
+});
+
+$('body').scrollspy({target: '#main-navbar'});
+
+var scrollMenu = function (idElement) {
+    var $targetElement = $(idElement);
+    var position = $targetElement.offset().top;
+    $('html, body').animate({scrollTop: position}, 3000);
+};
+
+$('.navbar a, .top a').on('click', function () {
+   var idElement = $(this).attr('href');
+   scrollMenu(idElement);
 });
