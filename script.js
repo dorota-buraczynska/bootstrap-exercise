d5d7d89230b528da4f900829dbd57a876aa3232a
Dorota Buraczyńska <dorota.buraczynska@gmail.com>
2017-04-20 15:38:45 +0200

    finish style and index, add slideanim

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-/**
- * Created by dorcik on 18.04.17.
- */
+function initMap() {
+    var position = {lat: 41.875329, lng:  -87.627811};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: position,
+        zoomControl: true,
+        scaleControl: false,
+        mapTypeControl: true,
+        fullscreenControl: true,
+        streetViewControl: true
+    });
+    var marker = new google.maps.Marker({
+        position: position,
+        map: map
+    });
+}
+
+$(window).scroll(function () {
+   $('.slideanim').each(function () {
+     var position = $(this).offset().top;
+     var winTop = $(window).scrollTop();
+     if (position < winTop + 600) {
+         $(this).addClass('slide');
+     }
+   });
+});
